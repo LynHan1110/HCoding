a85d61b80abac1a0b7aed9238f2aa9e5ef15a651
Lyn Han <103019970+LynHan1110@users.noreply.github.com>
2022-12-26 20:37:42 +0900

    Add files via upload

---
+++ scripts/application_server.js
@@ -0,0 +1,3 @@
+console.log("%c H Coding Log : lecture fetching complete.","color:#646E7E; background:#ef306aff;");
+console.log("%c H Coding Log : HTML, CSS, JavaScript, Node.js, Python lecture fetched.","color:#646E7E; background:#ef306aff;");
+
